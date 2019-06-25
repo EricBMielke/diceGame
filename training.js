f87fecff80b2c6169b001988caa88b0337a3b0d7
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-06-25 08:35:55 -0500

    Another training prompt added and functionality to return to main page is now added to the training page

--- training.js
+++ training.js
@@ -4,11 +4,27 @@ let continentTraining = true;
 while (continentTraining){
     let continentTrainingAnswer = window.prompt ("The median value for most of the continents is 6, if you have a continent with a danger value of 8, should you pick again? (yes/no)");
     if (continentTrainingAnswer == "yes" || continentTrainingAnswer == "Yes"){
-        window.alert ('Correct')
+        window.alert ('Correct');
         continentTraining = false;
     }
     else {
         window.alert ('Incorrect');
     }
-
 }
+let experienceTraining = true;
+while (experienceTraining){
+    let experienceTrainingAnswer = window.prompt ("It is much more likely for an experienced traveler to be successful in an unfamiliar situation. If an individual has 10 experience or 3 experience, who is more likely to be successful? (3/10)");
+    if (experienceTrainingAnswer == '10'){
+        window.alert('Correct');
+        experienceTraining =false;
+    }
+    else {
+        window.alert('Incorrect');
+    }
+}
+window.alert ('You are ready to return');
+window.location.href = "C:/Users/Eric/Documents/devCodeCamp/diceGame/index.html"
+
+
+    
+
