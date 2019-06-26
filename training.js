80f8515dda461336bade15f43c5b994752c4ece7
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-06-26 14:41:29 -0500

    Updating the style of the button and taking away unneccessary components from v1 of my dice game

--- training.js
+++ training.js
@@ -1,20 +1,9 @@
 //TRAINING FOR DISASTER SIMULATOR
 window.alert ('Welcome to survival 101');
-let continentTraining = true;
-while (continentTraining){
-    let continentTrainingAnswer = window.prompt ("The median value for most of the continents is 6, if you have a continent with a danger value of 8, should you pick again? (yes/no)");
-    if (continentTrainingAnswer == "yes" || continentTrainingAnswer == "Yes"){
-        window.alert ('Correct');
-        continentTraining = false;
-    }
-    else {
-        window.alert ('Incorrect');
-    }
-}
 let experienceTraining = true;
 while (experienceTraining){
-    let experienceTrainingAnswer = window.prompt ("It is much more likely for an experienced traveler to be successful in an unfamiliar situation. If an individual has 10 experience or 3 experience, who is more likely to be successful? (3/10)");
-    if (experienceTrainingAnswer == '10'){
+    let experienceTrainingAnswer = window.prompt ("It is much more likely for an experienced traveler to be successful in an unfamiliar situation. Is it likely that ");
+    if (experienceTrainingAnswer == 'yes' || experienceTrainingAnswer == 'Yes'){
         window.alert('Correct');
         experienceTraining =false;
     }
