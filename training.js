0f89ccc8a5adeef68533eb62a59474c6a6b48ac5
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-06-27 07:12:00 -0500

    Completing training mode by adding new questions and fixing broken variables

--- training.js
+++ training.js
@@ -2,10 +2,21 @@
 window.alert ('Welcome to survival 101');
 let experienceTraining = true;
 while (experienceTraining){
-    let experienceTrainingAnswer = window.prompt ("It is much more likely for an experienced traveler to be successful in an unfamiliar situation. Is it likely that ");
+    let experienceTrainingAnswer = window.prompt ("It is much more likely for an experienced traveler to be successful in an unfamiliar situation. Is it likely that when someone has more experience, they are able to handle the situation? ");
     if (experienceTrainingAnswer == 'yes' || experienceTrainingAnswer == 'Yes'){
         window.alert('Correct');
         experienceTraining =false;
+    }
+    else {
+        window.alert('Incorrect');
+    }
+}
+let vetTraining = true;
+while (vetTraining){
+    let experienceTrainingAnswer = window.prompt ("Are veterans more likely to survive high-stress situations than the common person?");
+    if (experienceTrainingAnswer == 'yes' || experienceTrainingAnswer == 'Yes'){
+        window.alert('Correct');
+        vetTraining =false;
     }
     else {
         window.alert('Incorrect');
